833145bc11b8ae21c7a778d1e03e161ed5712d98
Eduardo Ongaratto <ongarattodudu@gmail.com>
2024-06-10 11:38:25 -0300

    Primeiro commit do Desafio

---
+++ README.md
@@ -0,0 +1,31 @@
+# Programa para Gerar uma Sequências de Bolas
+
+Este programa foi desenvolvido em JavaScript e Node.js. Ele gera sequências de bolas com cores aleatórias com base em suas probabilidades e armazena essas sequências em um arquivo de texto. 
+## Como Rodar o Programa
+
+Para rodar o programa, siga estas estas etapas:
+
+1. **Instalar o VSCode:**
+   Certifique-se de ter o Visual Studio Code (VSCode) instalado em seu computador. Você pode baixá-lo e instalá-lo a partir do [site oficial do VSCode](https://code.visualstudio.com/).
+
+1. **Instalar o Node.js:**
+   Certifique-se de ter o Node.js instalado em seu computador. Você pode baixá-lo e instalá-lo a partir do [site oficial do Node.js](https://nodejs.org/).
+
+2. **Acessar o Diretório do Projeto:**
+   - Navegue para o diretório do projeto usando o terminal:
+     ```
+     cd nome-do-repositorio
+     ```
+
+4. **Rodar o Programa:**
+   - Após, você pode rodar o programa com o seguinte comando:
+     ```
+     node index.js
+     ```
+
+5. **Verificar Resultados:**
+   - O programa irá gerar um arquivo chamado `resultado.txt` contendo as sequências de bolas.
+   - Os resultados da contagem de cores serão exibidos no terminal do vscode.
+
+## Personalização do Programa (Opcional)
+Você pode personalizar o programa alterando os parâmetros, como o número de sequências a serem geradas ou as cores e pesos das bolas.
---
+++ index.js
@@ -0,0 +1,65 @@
+//importar modulo interno (core modulo) fs para escrever em um arquivo txt
+const fs = require("fs");
+
+//criando as bolas e suas probabilidades
+const bolas = [
+  { cor: "verde", peso: 1 },
+  { cor: "azul", peso: 2 },
+  { cor: "amarelo", peso: 3 },
+  { cor: "vermelho", peso: 5 },
+];
+
+//função para escolher uma bola de acordo com a quantida (pesos)
+function escolherBola() {
+  const totalPeso = bolas.reduce((total, bola) => total + bola.peso, 0);
+  const escolha = Math.random() * totalPeso;
+  let somaPeso = 0;
+
+  for (let i = 0; i < bolas.length; i++) {
+    somaPeso += bolas[i].peso;
+    if (escolha < somaPeso) {
+      return bolas[i].cor;
+    }
+  }
+}
+
+// Função para gerar a sequencia de 4 bolas
+function gerararrSequencia() {
+  let arrSequencia = [];
+  for (let i = 0; i < 4; i++) {
+    arrSequencia.push(escolherBola());
+  }
+  return arrSequencia.join(",");
+}
+
+// Gerar a sequências e escrever no arquivo
+let = quantidadeDaSequencia = 1000;
+let arrSequencias = "";
+for (let i = 0; i < quantidadeDaSequencia; i++) {
+  arrSequencias += gerararrSequencia() + "\n";
+}
+
+fs.writeFileSync("resultado.txt", arrSequencias.trim());
+
+// Ler o arquivo e contabilizar as cores escolhidas
+const conteudo = fs.readFileSync("resultado.txt", "utf-8");
+const linhas = conteudo.split("\n");
+
+const contagem = {
+  verde: 0,
+  azul: 0,
+  amarelo: 0,
+  vermelho: 0,
+};
+
+for (let i = 0; i < linhas.length; i++) {
+  const cores = linhas[i].split(",");
+  for (let j = 0; j < cores.length; j++) {
+    contagem[cores[j]]++;
+  }
+}
+
+// Exibir o resultado no console
+for (let cor in contagem) {
+  console.log(cor + ": " + contagem[cor]);
+}
